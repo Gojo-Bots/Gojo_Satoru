0dd0b1d164c7c3b0689a9fc04f360bd4d24a143c
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-04-07 08:30:44 +0530

    Update fun.py

--- Powers/plugins/fun.py
+++ Powers/plugins/fun.py
@@ -198,6 +198,7 @@ _DISABLE_CMDS_ = [
 
 __HELP__ = """× /runs: reply a random string from an array of replies.
 × /slap: slap a user, or get slapped if not a reply.
+× /insult: to insult a user, or get insulted if not a reply
 × /shrug : get shrug XD.
 × /decide : Randomly answers yes/no/maybe
 × /toss : Tosses A coin
