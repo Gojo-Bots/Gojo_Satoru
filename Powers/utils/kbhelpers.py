6ee067f4d015b8f736b71ddea1f66cc44e4b3d1c
iamgojoof6eyes <iamgojoof6eyes@gmail.com>
2022-08-21 10:29:15 +0530

    Encountered problem while doing changes `reverted`

--- Powers/utils/kbhelpers.py
+++ Powers/utils/kbhelpers.py
@@ -1,42 +1,19 @@
-from re import findall
-from pykeyboard import InlineKeyboard
-from pyrogram.types import InlineKeyboardButton as kb
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-# CREDIT WILLIAM BUTCHER BOT
+def ikb(rows=None):
+    if rows is None:
+        rows = []
+    lines = []
+    for row in rows:
+        line = []
+        for button in row:
+            button = btn(*button)  # InlineKeyboardButton
+            line.append(button)
+        lines.append(line)
+    return InlineKeyboardMarkup(inline_keyboard=lines)
 
 
-def is_url(text: str) -> bool:
-    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
-                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
-                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
-                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
-    return [x[0] for x in findall(regex, str(text))]
-
-
-def keyboard(buttons_list, row_width: int = 2):
-    """
-    Buttons builder, pass buttons in a list and it will
-    return pyrogram.types.IKB object
-    Ex: keyboard([["click here", "https://google.com"]])
-    if theres, a url, it will make url button, else callback button
-    """
-    buttons = InlineKeyboard(row_width=row_width)
-    data = [
-        (
-            kb(text=str(i[0]), callback_data=str(i[1]))
-            if not is_url(i[1])
-            else kb(text=str(i[0]), url=str(i[1]))
-        )
-        for i in buttons_list
-    ]
-    buttons.add(*data)
-    return buttons
-
-
-def ikb(data: dict, row_width: int = 2):
-    """
-    Converts a dict to pyrogram buttons
-    Ex: dict_to_keyboard({"click here": "this is callback data"})
-    """
-    return keyboard(data.items(), row_width=row_width)
+def btn(text, value, type="callback_data"):
+    return InlineKeyboardButton(text, **{type: value})
