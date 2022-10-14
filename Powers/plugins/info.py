d2992fb3750ee3d2c98f873800b1c12d104dee58
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-10-14 20:50:31 +0530

    Update info.py

--- Powers/plugins/info.py
+++ Powers/plugins/info.py
@@ -72,8 +72,8 @@ async def user_info(c: Gojo, user, already=False):
             omp = "User is sudoer"
         elif user_id in WHITELIST_USERS:
             omp = "User is in whitelist"
-        else:
-            omp = "Hmmm.......Who is that again?"
+     
+    omp = "Hmmm.......Who is that again?"
     is_bot = user.is_bot
     is_fake = user.is_fake
     status = user.status
