969f1dcf75ac7d5bf4097f84778c6a7704c38ca7
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-10-27 19:47:54 +0530

    Update kbhelpers.py

--- Powers/utils/kbhelpers.py
+++ Powers/utils/kbhelpers.py
@@ -13,18 +13,25 @@ def ikb(rows=None, back=False, todo="start_back"):
     try:
         for row in rows:
             line = []
-            for button in row:
-                btn_text = button.split(".")[1].upper()
-                button = btn(btn_text, button)  # InlineKeyboardButton
-                line.append(button)
-            lines.append(line)
-    except (AttributeError, TypeError): # Will make the kb which don't have "." in them
+            try:
+                for button in row:
+                    btn_text = button.split(".")[1].upper()
+                    button = btn(btn_text, button)  # InlineKeyboardButton
+                    line.append(button)
+                lines.append(line)
+            except AttributeError:
+                for button in rows:
+                    button = btn(*button)  # Will make the kb which don't have "." in them
+                    line.append(button)
+                lines.append(line)
+    except TypeError:
+        # make a code to handel that error
         line = []
         for button in rows:
             button = btn(*button)  # InlineKeyboardButton
             line.append(button)
         lines.append(line)
-    if back:
+    if back: 
         back_btn = [(btn("« Back", todo))]
         lines.append(back_btn)
     return InlineKeyboardMarkup(inline_keyboard=lines)
