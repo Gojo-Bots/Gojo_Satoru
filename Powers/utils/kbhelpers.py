caa981e7412f5ed3098c930bbf1a464e5986467e
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-10-27 18:02:48 +0530

    Update kbhelpers.py

--- Powers/utils/kbhelpers.py
+++ Powers/utils/kbhelpers.py
@@ -2,22 +2,27 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def ikb(rows=None, back=False, todo="start_back"):
+    """
+    rows = pass the rows
+    back - if want to make back button
+    todo - callback data of back button
+    """
     if rows is None:
         rows = []
     lines = []
-    for row in rows:
-        line = []
-        try:
+    try:
+        for row in rows:
+            line = []
             for button in row:
                 btn_text = button.split(".")[1].upper()
                 button = btn(btn_text, button)  # InlineKeyboardButton
                 line.append(button)
             lines.append(line)
-        except AttributeError:
-            for button in row:
-                button = btn(*button)  # InlineKeyboardButton
-                line.append(button)
-            lines.append(line)
+    except (AttributeError, TypeError): # Will make the kb which don't have "." in them
+        for button in row:
+            button = btn(*button)  # InlineKeyboardButton
+            line.append(button)
+        lines.append(line)
     if back:
         back_btn = [(btn("« Back", todo))]
         lines.append(back_btn)
