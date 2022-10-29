166b26ef7cd6c134ce5a70f4c93460f5428e0f45
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-10-30 00:25:17 +0530

    Update utils.py

--- Powers/plugins/utils.py
+++ Powers/plugins/utils.py
@@ -296,7 +296,7 @@ async def paste_func(_, message: Message):
         await m.delete()
         await message.reply_text("Here's your paste", reply_markup=InlineKeyboardMarkup(kb))
     except Exception:
-        await message.reply_text(f"Here's your paste [link]({link})",)
+        await message.reply_text(f"Here's your paste:\n {link}",)
     return
 
 @Gojo.on_message(command("tr"))
