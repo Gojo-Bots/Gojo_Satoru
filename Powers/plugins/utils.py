27dfaedcdbf6a7242287c1e78cb031c57c4a6c48
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-10-30 15:04:17 +0530

    Update utils.py

--- Powers/plugins/utils.py
+++ Powers/plugins/utils.py
@@ -255,7 +255,7 @@ async def paste(content: str):
     resp = await post(f"{BASE}api/v2/paste", data=content)
     if not resp["success"]:
         return
-    return BASE + resp["id"]
+    return BASE + resp["message"]
 
 
 @Gojo.on_message(command("paste"))
