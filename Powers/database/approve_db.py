b420816610c499cc01a913d81fbdc69c01be5987
Captain Ezio <iamgojoof6eyes@gmail.com>
2023-08-25 08:08:28 +0530

    Update approve_db.py

--- Powers/database/approve_db.py
+++ Powers/database/approve_db.py
@@ -13,7 +13,16 @@ class Approve(MongoDB):
 
     def check_approve(self, user_id: int):
         with INSERTION_LOCK:
-            return bool(user_id in i for i in self.chat_info["users"])
+            if not self.chat_info["users"]:
+                return False
+            for i in self.chat_info["users"]:
+                if user_id in i:
+                    j = True
+                    break
+                else:
+                    j = False
+            return j
+                    
 
     def add_approve(self, user_id: int, user_name: str):
         with INSERTION_LOCK:
