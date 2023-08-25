4c895444a8214900a29c4f90ec6cb4b9df494dfa
Captain Ezio <iamgojoof6eyes@gmail.com>
2023-08-25 08:05:29 +0530

    Update approve_db.py

--- Powers/database/approve_db.py
+++ Powers/database/approve_db.py
@@ -1,17 +1,11 @@
 from threading import RLock
-
 from Powers import LOGGER
 from Powers.database import MongoDB
-
 INSERTION_LOCK = RLock()
-
-
 class Approve(MongoDB):
     """Class for managing Approves in Chats in Bot."""
-
     # Database name to connect to to preform operations
     db_name = "approve"
-
     def __init__(self, chat_id: int) -> None:
         super().__init__(self.db_name)
         self.chat_id = chat_id
@@ -19,14 +13,7 @@ class Approve(MongoDB):
 
     def check_approve(self, user_id: int):
         with INSERTION_LOCK:
-            j = False
-            if not self.chat_info["users"]:
-                return j
-            for i in self.chat_info["users"]:
-                if user_id in i:
-                    j = True
-                    break
-            return j
+            return bool(user_id in i for i in self.chat_info["users"])
 
     def add_approve(self, user_id: int, user_name: str):
         with INSERTION_LOCK:
@@ -37,7 +24,6 @@ class Approve(MongoDB):
                     {"users": self.chat_info["users"]},
                 )
             return True
-
     def remove_approve(self, user_id: int):
         with INSERTION_LOCK:
             if self.check_approve(user_id):
@@ -52,30 +38,24 @@ class Approve(MongoDB):
                     {"users": self.chat_info["users"]},
                 )
             return True
-
     def unapprove_all(self):
         with INSERTION_LOCK:
             return self.delete_one(
                 {"_id": self.chat_id},
             )
-
     def clean_approve(self):
         with INSERTION_LOCK:
             return self.delete_one(
                 {"_id":self.chat_id}
             )
-
     def list_approved(self):
         with INSERTION_LOCK:
             return self.chat_info["users"]
-
     def count_approved(self):
         with INSERTION_LOCK:
             return len(self.chat_info["users"])
-
     def load_from_db(self):
         return self.find_all()
-
     def __ensure_in_db(self):
         chat_data = self.find_one({"_id": self.chat_id})
         if not chat_data:
@@ -84,28 +64,24 @@ class Approve(MongoDB):
             LOGGER.info(f"Initialized Approve Document for chat {self.chat_id}")
             return new_data
         return chat_data
-
     # Migrate if chat id changes!
     def migrate_chat(self, new_chat_id: int):
         old_chat_db = self.find_one({"_id": self.chat_id})
         new_data = old_chat_db.update({"_id": new_chat_id})
         self.insert_one(new_data)
         self.delete_one({"_id": self.chat_id})
-
     @staticmethod
     def count_all_approved():
         with INSERTION_LOCK:
             collection = MongoDB(Approve.db_name)
             all_data = collection.find_all()
             return sum(len(i["users"]) for i in all_data if len(i["users"]) >= 1)
-
     @staticmethod
     def count_approved_chats():
         with INSERTION_LOCK:
             collection = MongoDB(Approve.db_name)
             all_data = collection.find_all()
             return sum(len(i["users"]) >= 1 for i in all_data)
-
     @staticmethod
     def repair_db(collection):
         all_data = collection.find_all()
