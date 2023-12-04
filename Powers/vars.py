0e8b81854d6d502d454f43767698d54920611745
Captain D. Ezio <iamgojoof6eyes@gmail.com>
2023-12-04 19:18:59 +0530

    ehehehe

--- Powers/vars.py
+++ Powers/vars.py
@@ -21,21 +21,21 @@ class Config:
         for i in config(
             "DEV_USERS",
             default="",
-        ).split(" ")
+        ).split(None)
     ]
     SUDO_USERS = [
         int(i)
         for i in config(
             "SUDO_USERS",
             default="",
-        ).split(" ")
+        ).split(None)
     ]
     WHITELIST_USERS = [
         int(i)
         for i in config(
             "WHITELIST_USERS",
             default="",
-        ).split(" ")
+        ).split(None)
     ]
     GENIUS_API_TOKEN = config("GENIUS_API",default=None)
     AuDD_API = config("AuDD_API",default=None)
