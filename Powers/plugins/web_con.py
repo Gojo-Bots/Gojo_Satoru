91c2a1ec0b64c529173f37ab36c368ab61347aed
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-04-05 20:52:12 +0530

    Update web_con.py

--- Powers/plugins/web_con.py
+++ Powers/plugins/web_con.py
@@ -308,7 +308,7 @@ async def download_instareels(c: Gojo, m: Message):
     except IndexError:
         await m.reply_text("Give me an link to download it...")
         return
-    if not reel_.startswith("https://www.ddinstagram.com/reel/"):
+    if not reel_.startswith("https://www.instagram.com/reel/"):
         await m.reply_text("In order to obtain the requested reel, a valid link is necessary. Kindly provide me with the required link.")
         return
     OwO = reel_.split(".",1)
