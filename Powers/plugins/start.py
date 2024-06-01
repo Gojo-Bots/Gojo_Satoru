75e09c2a1a920d758fc80da32ebfd616b3745ead
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-06-02 04:08:29 +0530

    Update start.py

--- Powers/plugins/start.py
+++ Powers/plugins/start.py
@@ -32,7 +32,7 @@ Your donation might also me get me a new feature or two, which I wasn't able to 
 
 All the fund would be put into my services such as database, storage and hosting!
 
-You can donate by contacting my owner: [Captain Ezio](http://t.me/iamgojoof6eyes)
+You can donate by contacting my owner: [Tosu](http://t.me/its_damiann)
      """
 
     LOGGER.info(f"{m.from_user.id} fetched donation text in {m.chat.id}")
@@ -111,7 +111,7 @@ Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Gojo âœ
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](https://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join my [News Channel](https://t.me/about_tosuu) to get information on all the latest updates."""
 
             await m.reply_photo(
                 photo=str(choice(StartPic)),
@@ -150,7 +150,7 @@ Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Gojo âœ
 I'm here to help you manage your group(s)!
 Hit /help to find out more about how to use me in my full potential!
 
-Join my [News Channel](http://t.me/gojo_bots_network) to get information on all the latest updates."""
+Join my [News Channel](https://t.me/about_tosuu) to get information on all the latest updates."""
 
         await q.edit_message_caption(
             caption=cpt,
