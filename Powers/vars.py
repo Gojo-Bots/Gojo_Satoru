e54b933cb1237521f5bf9fccccbff1ad22712576
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-08-20 22:01:41 +0530

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -11,44 +11,27 @@ class Config:
     """Config class for variables."""
 
     LOGGER = True
-    BOT_TOKEN = config("6924002790:AAHaWLtKzjzPBLo6ZovJvBdh6C1I2tLz1d4", default=None)
-    API_ID = int(config("API_ID", default="123"))
-    API_HASH = config("API_HASH", default=None)
-    OWNER_ID = int(config("OWNER_ID", default=1344569458))
-    MESSAGE_DUMP = int(config("MESSAGE_DUMP"))
-    DEV_USERS = [
-        int(i)
-        for i in config(
-            "DEV_USERS",
-            default="",
-        ).split(None)
-    ]
-    SUDO_USERS = [
-        int(i)
-        for i in config(
-            "SUDO_USERS",
-            default="",
-        ).split(None)
-    ]
-    WHITELIST_USERS = [
-        int(i)
-        for i in config(
-            "WHITELIST_USERS",
-            default="",
-        ).split(None)
-    ]
-    GENIUS_API_TOKEN = config("GENIUS_API",default=None)
-    AuDD_API = config("AuDD_API",default=None)
-    RMBG_API = config("RMBG_API",default=None)
-    DB_URI = config("DB_URI", default="")
-    DB_NAME = config("DB_NAME", default="gojo_satarou")
-    BDB_URI = config("BDB_URI",default=None)
-    NO_LOAD = config("NO_LOAD", default="").split()
-    PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="gojo_bots_network")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="gojo_bots_network")
-    WORKERS = int(config("WORKERS", default=16))
-    TIME_ZONE = config("TIME_ZONE",default='Asia/Kolkata')
+    BOT_TOKEN = "hajsfhuhj:123455...."
+    API_ID = 12345  # Your APP_ID from Telegram
+    API_HASH = "asjhfuahf7572hjgkaghdf"  # Your APP_HASH from Telegram
+    OWNER_ID = 1344569458  # Your telegram user id defult to mine
+    MESSAGE_DUMP = -100845454887  # Your Private Group ID for logs
+    DEV_USERS = [123456667]
+    SUDO_USERS = [2345123123]
+    WHITELIST_USERS = [12314134]
+    DB_URI = "mongodb+srv://User:testdb.m14k3kx.mongodb.net/?retryWrites=true&w=majority"  # Your mongo DB URI
+    DB_NAME = "MYDB"  # Your DB name
+    NO_LOAD = []
+    GENIUS_API_TOKEN = "" # Your genius API token or leave it as it is
+    RMBG_API = "" # Your rmbg API token or leave it as it is
+    PREFIX_HANDLER = ["!", "/","$"]
+    SUPPORT_GROUP = "gojo_bots_network" #Username without @
+    SUPPORT_CHANNEL = "gojo_bots_network" #Username without @
+    VERSION = "VERSION" #Leave it as it is
+    TIME_ZONE = 'Asia/Kolkata'
+    BDB_URI = "" #If you want your birthday module to work pass mongo db uri u can use same URI but I prefer passing a new one
+    WORKERS = 8
+    TIME_ZONE = 'Asia/Kolkata'
     BOT_USERNAME = ""
     BOT_ID = ""
     BOT_NAME = ""
@@ -60,23 +43,23 @@ class Development:
 
     # Fill in these vars if you want to use Traditional method of deploying
     LOGGER = True
-    BOT_TOKEN = "YOUR BOT_TOKEN"
+    BOT_TOKEN = "6924002790:AAHaWLtKzjzPBLo6ZovJvBdh6C1I2tLz1d4"
     API_ID = 24269862  # Your APP_ID from Telegram
     API_HASH = "5b1a646f8c8ed40f15af84c9b2dfa9e8"  # Your APP_HASH from Telegram
     OWNER_ID = 5154912723  # Your telegram user id defult to mine
     MESSAGE_DUMP = -1002052189895  # Your Private Group ID for logs
     DEV_USERS = [5154912723]
-    SUDO_USERS = []
-    WHITELIST_USERS = []
+    SUDO_USERS = [5154912723]
+    WHITELIST_USERS = [5154912723]
     DB_URI = "mongodb+srv://yumlanulmi:hii121itsk@cluster0.gbperk3.mongodb.net/?retryWrites=true&w=majority"  # Your mongo DB URI
     DB_NAME = "Management"  # Your DB name
     NO_LOAD = []
-    GENIUS_API_TOKEN = ""
-    RMBG_API = "W6KWsRHiQCxedSEvfSLY2Mex"
+    GENIUS_API_TOKEN = "" # Your genius API token or leave it as it is
+    RMBG_API = "W6KWsRHiQCxedSEvfSLY2Mex" # Your rmbg API token or leave it as it is
     PREFIX_HANDLER = ["!", "/","$"]
-    SUPPORT_GROUP = "Lundlelobsdkmera"
-    SUPPORT_CHANNEL = "Noxbots"
-    VERSION = "VERSION"
+    SUPPORT_GROUP = "NoxBots" #Username without @
+    SUPPORT_CHANNEL = "Lundlelobsdkmera" #Username without @
+    VERSION = "VERSION" #Leave it as it is
     TIME_ZONE = 'Asia/Kolkata'
-    BDB_URI = "mongodb+srv://b7604190:hii121itsk@cluster0.vtt1cxt.mongodb.net/?retryWrites=true&w=majority"
+    BDB_URI = "mongodb+srv://b7604190:hii121itsk@cluster0.vtt1cxt.mongodb.net/?retryWrites=true&w=majority" #If you want your birthday module to work pass mongo db uri u can use same URI but I prefer passing a new one
     WORKERS = 8
