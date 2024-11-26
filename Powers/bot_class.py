aba3892c2393e6827949f5824c39ab8574f5dfc2
ʜʀɪᴅᴏʏ <itsmehhridoy@gmail.com>
2024-11-26 22:34:13 +0600

    Update bot_class.py

--- Powers/bot_class.py
+++ Powers/bot_class.py
@@ -43,15 +43,6 @@ class Gojo(Client):
     async def start(self):
         """Start the bot."""
         await super().start()
-        await self.set_bot_commands(
-            [
-                BotCommand(
-                    "start", "To check weather the bot is alive or not"),
-                BotCommand("help", "To get help menu"),
-                BotCommand("donate", "To buy me a coffee"),
-                BotCommand("bug", "To report bugs")
-            ]
-        )
         meh = await self.get_me()  # Get bot info from pyrogram client
         LOGGER.info("Starting bot...")
         Config.BOT_ID = meh.id
