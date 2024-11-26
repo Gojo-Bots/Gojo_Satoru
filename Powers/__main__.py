60430d83025fdc9336de78b43038db62ca47203c
ʜʀɪᴅᴏʏ <itsmehhridoy@gmail.com>
2024-11-26 22:39:49 +0600

    Update __main__.py

--- Powers/__main__.py
+++ Powers/__main__.py
@@ -1,19 +1,5 @@
-import os
-from platform import system
-
 from Powers import LOGGER
 from Powers.bot_class import Gojo
 
 if __name__ == "__main__":
-    if system() == "Windows":
-        LOGGER.info("Windows system detected thus not installing uvloop")
-    else:
-        LOGGER.info("Attempting to install uvloop")
-        try:
-            os.system("pip3 install uvloop==0.19.0")
-            import uvloop
-            uvloop.install()
-            LOGGER.info("Installed uvloop continuing the process")
-        except:
-            LOGGER.info("Failed to install uvloop continuing the process")
     Gojo().run()
