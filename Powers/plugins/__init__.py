f0ef36dca85619ecd69c4a9a487b2732ae734c37
ʜʀɪᴅᴏʏ <itsmehhridoy@gmail.com>
2024-11-26 22:30:04 +0600

    Update __init__.py

--- Powers/plugins/__init__.py
+++ Powers/plugins/__init__.py
@@ -13,25 +13,6 @@ async def all_plugins():
     ]
     return sorted(all_plugs)
 
-
-from sys import exit as exiter
-
-from pymongo import MongoClient
-from pymongo.errors import PyMongoError
-
-from Powers import BDB_URI, LOGGER
-
-if BDB_URI:
-    try:
-        BIRTHDAY_DB = MongoClient(BDB_URI)
-    except PyMongoError as f:
-        LOGGER.error(f"Error in Mongodb2: {f}")
-        exiter(1)
-    Birth_main_db = BIRTHDAY_DB["birthdays"]
-
-    bday_info = Birth_main_db['users_bday']
-    bday_cinfo = Birth_main_db["chat_bday"]
-
 from datetime import datetime
 
 
